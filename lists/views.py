9d4d46b2e6d24d282ede262a17356d8042f4c3e4
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-08-31 20:21:01 -0300

    Ciclo: Teste de Unidade/Desenvolvimento

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<html><title>To-Do</title></html>")
+    return render(request, "lists/home.html")
