d08158ba5eaae4dbab6efa7556c8311aeb36fd97
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-09-09 19:34:02 -0300

    Utilizando Selenium para Testar Interações do Usuário

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,6 +16,28 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get("http://localhost:8000")
 
         self.assertIn("To-Do", self.browser.title)
+
+        header_text = self.browser.find_element("tag name", "h1").text
+        self.assertIn("Your To-Do list", header_text)
+
+        inputbox = self.browser.find_element("id", "id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
+
+        inputbox.send_keys("Estudar testes funcionais")
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        table = self.browser.find_element("id", "id_list_table")
+        rows = table.find_elements("tag name", "tr")
+
+        self.assertTrue(
+            any(row.text == '1: Estudar testes funcionais' for row in rows),
+            "New to-do item did not appear in table"
+        )
 
 
 if __name__ == "__main__":
