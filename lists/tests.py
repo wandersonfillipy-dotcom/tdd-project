c1f907340b82d552185968ddbbe0bde56b4ad408
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-09-09 19:52:31 -0300

    Utilizando o Django Test Client

--- lists/tests.py
+++ lists/tests.py
@@ -3,12 +3,6 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
+    def test_uses_home_template(self):
         response = self.client.get("/")
-
-        self.assertEqual(response.status_code, 200)
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get("/")
-
-        self.assertContains(response, "To-Do")
+        self.assertTemplateUsed(response, "home.html")
