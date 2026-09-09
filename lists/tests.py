bf7e52492e8c507ee19cef70803c17b06569b586
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-09-09 20:33:59 -0300

    Salvando o POST no Banco de Dados

--- lists/tests.py
+++ lists/tests.py
@@ -14,6 +14,11 @@ class HomePageTest(TestCase):
             data={"item_text": "A new list item"}
         )
 
+        self.assertEqual(Item.objects.count(), 1)
+
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
         self.assertIn(
             "A new list item",
             response.content.decode()
