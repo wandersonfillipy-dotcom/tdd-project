df5611521ea5aae02b4a0cc0649ccd18f9de9c25
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-09-09 20:31:54 -0300

    Persistindo os Dados

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -19,3 +20,24 @@ class HomePageTest(TestCase):
         )
 
         self.assertTemplateUsed(response, "home.html")
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "O primeiro item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "O segundo item"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, "O primeiro item")
+        self.assertEqual(second_saved_item.text, "O segundo item")
