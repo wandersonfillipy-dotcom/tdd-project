59c99cc1ac32d95661245f7273ebcc81182d3b1a
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-09-09 20:35:50 -0300

    Não Salvar Itens em Branco a cada Requisição

--- lists/tests.py
+++ lists/tests.py
@@ -26,6 +26,10 @@ class HomePageTest(TestCase):
 
         self.assertTemplateUsed(response, "home.html")
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
 
