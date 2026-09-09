a076d28b2429ec1c398fd4ff56be4fbd58e22eea
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-09-09 20:26:20 -0300

    Refatorando o Teste Funcional

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element("id", "id_list_table")
+        rows = table.find_elements("tag name", "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
 
@@ -31,12 +36,8 @@ class NewVisitorTest(unittest.TestCase):
 
         time.sleep(1)
 
-        table = self.browser.find_element("id", "id_list_table")
-        rows = table.find_elements("tag name", "tr")
-
-        self.assertIn(
-            "1: Estudar testes funcionais",
-            [row.text for row in rows]
+        self.check_for_row_in_list_table(
+            "1: Estudar testes funcionais"
         )
 
         inputbox = self.browser.find_element("id", "id_new_item")
@@ -45,17 +46,12 @@ class NewVisitorTest(unittest.TestCase):
 
         time.sleep(1)
 
-        table = self.browser.find_element("id", "id_list_table")
-        rows = table.find_elements("tag name", "tr")
-
-        self.assertIn(
-            "1: Estudar testes funcionais",
-            [row.text for row in rows]
+        self.check_for_row_in_list_table(
+            "1: Estudar testes funcionais"
         )
 
-        self.assertIn(
-            "2: Estudar testes de unidade",
-            [row.text for row in rows]
+        self.check_for_row_in_list_table(
+            "2: Estudar testes de unidade"
         )
 
 
