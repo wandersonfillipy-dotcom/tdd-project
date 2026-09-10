ce61dfde54d650e65c4f977008f10007144bdecb
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-09-09 21:47:37 -0300

    Implementando o Novo Projeto (Listas e Itens) - PARTE 5

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,14 +1,15 @@
-from django.test import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
 import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
 
 
 MAX_WAIT = 5
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,40 +22,86 @@ class NewVisitorTest(LiveServerTestCase):
 
         while True:
             try:
-                table = self.browser.find_element("id", "id_list_table")
-                rows = table.find_elements("tag name", "tr")
-                self.assertIn(row_text, [row.text for row in rows])
+                table = self.browser.find_element(
+                    "id",
+                    "id_list_table"
+                )
+
+                rows = table.find_elements(
+                    "tag name",
+                    "tr"
+                )
+
+                self.assertIn(
+                    row_text,
+                    [row.text for row in rows]
+                )
+
                 return
+
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
+
                 time.sleep(0.5)
 
     def test_can_start_a_list_for_one_user(self):
-        # Maria decidiu utilizar o novo app TODO.
-        # Ela entra em sua página principal.
         self.browser.get(self.live_server_url)
 
-        self.assertIn("To-Do", self.browser.title)
+        self.assertIn(
+            "To-Do",
+            self.browser.title
+        )
 
-        header_text = self.browser.find_element("tag name", "h1").text
-        self.assertIn("Your To-Do list", header_text)
+        header_text = self.browser.find_element(
+            "tag name",
+            "h1"
+        ).text
 
-        inputbox = self.browser.find_element("id", "id_new_item")
+        self.assertIn(
+            "Start a new To-Do list",
+            header_text
+        )
+
+        inputbox = self.browser.find_element(
+            "id",
+            "id_new_item"
+        )
+
         self.assertEqual(
             inputbox.get_attribute("placeholder"),
             "Enter a to-do item"
         )
 
-        inputbox.send_keys("Estudar testes funcionais")
+        inputbox.send_keys(
+            "Estudar testes funcionais"
+        )
+
         inputbox.send_keys(Keys.ENTER)
 
         self.wait_for_row_in_list_table(
             "1: Estudar testes funcionais"
         )
 
-        inputbox = self.browser.find_element("id", "id_new_item")
-        inputbox.send_keys("Estudar testes de unidade")
+        header_text = self.browser.find_element(
+            "tag name",
+            "h1"
+        ).text
+
+        self.assertIn(
+            "Your To-Do list",
+            header_text
+        )
+
+        inputbox = self.browser.find_element(
+            "id",
+            "id_new_item"
+        )
+
+        inputbox.send_keys(
+            "Estudar testes de unidade"
+        )
+
         inputbox.send_keys(Keys.ENTER)
 
         self.wait_for_row_in_list_table(
@@ -66,65 +113,69 @@ class NewVisitorTest(LiveServerTestCase):
         )
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        # Maria começa uma nova lista
         self.browser.get(self.live_server_url)
 
-        inputbox = self.browser.find_element("id", "id_new_item")
-        inputbox.send_keys("Estudar testes funcionais")
+        inputbox = self.browser.find_element(
+            "id",
+            "id_new_item"
+        )
+
+        inputbox.send_keys(
+            "Estudar testes funcionais"
+        )
+
         inputbox.send_keys(Keys.ENTER)
 
         self.wait_for_row_in_list_table(
             "1: Estudar testes funcionais"
         )
 
-        # Ela nota que sua lista possui uma URL única
         maria_list_url = self.browser.current_url
-        self.assertRegex(maria_list_url, "/lists/.+")
 
-        # Agora, um novo usuário, João, entra no site
+        self.assertRegex(
+            maria_list_url,
+            "/lists/.+"
+        )
+
         self.browser.quit()
+
         self.browser = webdriver.Firefox()
 
-        # João visita a página inicial.
-        # Não existe nenhum sinal da lista de Maria
         self.browser.get(self.live_server_url)
 
-        page_text = self.browser.find_element("tag name", "body").text
+        page_text = self.browser.find_element(
+            "tag name",
+            "body"
+        ).text
+
         self.assertNotIn(
-            "1: Estudar testes funcionais",
-            page_text
-        )
-        self.assertNotIn(
-            "2: Estudar testes de unidade",
+            "Estudar testes funcionais",
             page_text
         )
 
-        # João inicia uma nova lista
-        inputbox = self.browser.find_element("id", "id_new_item")
-        inputbox.send_keys("Comprar leite")
+        inputbox = self.browser.find_element(
+            "id",
+            "id_new_item"
+        )
+
+        inputbox.send_keys(
+            "Comprar leite"
+        )
+
         inputbox.send_keys(Keys.ENTER)
 
         self.wait_for_row_in_list_table(
             "1: Comprar leite"
         )
 
-        # João pega sua URL única
         joao_list_url = self.browser.current_url
-        self.assertRegex(joao_list_url, "/lists/.+")
+
+        self.assertRegex(
+            joao_list_url,
+            "/lists/.+"
+        )
+
         self.assertNotEqual(
             joao_list_url,
             maria_list_url
         )
-
-        # Novamente, não existe sinal da lista de Maria
-        page_text = self.browser.find_element("tag name", "body").text
-        self.assertNotIn(
-            "Estudar testes funcionais",
-            page_text
-        )
-        self.assertIn(
-            "Comprar leite",
-            page_text
-        )
-
-        # Satisfeitos, ambos vão dormir
--- lists/tests.py
+++ lists/tests.py
@@ -8,10 +8,6 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_only_saves_items_when_necessary(self):
-        self.client.get("/")
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ListViewTest(TestCase):
 
