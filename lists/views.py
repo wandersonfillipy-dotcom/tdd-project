f2276d900f95a59baf7b71f59989564465a4f8ce
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-09-09 21:39:50 -0300

    Implementando o Novo Projeto (Listas e Itens) - PARTE 4

--- lists/views.py
+++ lists/views.py
@@ -17,3 +17,13 @@ def view_list(request):
     return render(request, "list.html", {
         "items": items
     })
+
+
+def new_list(request):
+    Item.objects.create(
+        text=request.POST["item_text"]
+    )
+
+    return redirect(
+        "/lists/the-only-list-in-the-world/"
+    )
