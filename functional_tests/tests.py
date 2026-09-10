42c0ded26704d38d433e5017d28bf8a6e647e84e
Wanderson Fillipy da Silva Amaral <wanderson34215246@edu.df.senac.br>
2026-09-09 21:05:59 -0300

    Melhoria 2: Removendo Sleeps para Evitar Não Determinismo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,14 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from selenium.common.exceptions import WebDriverException
 import time
 
 
-class NewVisitorTest(unittest.TestCase):
+MAX_WAIT = 5
+
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -12,13 +16,22 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element("id", "id_list_table")
-        rows = table.find_elements("tag name", "tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.browser.find_element("id", "id_list_table")
+                rows = table.find_elements("tag name", "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         self.assertIn("To-Do", self.browser.title)
 
@@ -34,9 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Estudar testes funcionais")
         inputbox.send_keys(Keys.ENTER)
 
-        time.sleep(1)
-
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             "1: Estudar testes funcionais"
         )
 
@@ -44,16 +55,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Estudar testes de unidade")
         inputbox.send_keys(Keys.ENTER)
 
-        time.sleep(1)
-
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             "1: Estudar testes funcionais"
         )
 
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             "2: Estudar testes de unidade"
         )
-
-
-if __name__ == "__main__":
-    unittest.main()
